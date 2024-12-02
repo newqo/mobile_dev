8a82978596fcbf637a246a22449002435533660d
newqo <worawit.forwork@gmail.com>
2024-12-02 10:46:54 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_mobile_dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_mobile_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+  // runApp(const MaterialApp(home: Text('Hello World1!'),));
+}
+
+// stl shortcut
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Must have MaterialApp() to run
+
+      // remove debug banner
+      debugShowCheckedModeBanner: false,
+
+      // theme
+      theme: ThemeData(
+          useMaterial3: true,
+          appBarTheme: AppBarTheme(color: Colors.lightBlue)),
+
+      home: Scaffold(
+        // Scaffold is like form.
+        appBar: AppBar(
+          title: Text('Appbar title'),
+        ),
+        body: Text(
+            'Hello world'), // if have button or elements it will be paste in body.
+      ),
+    );
+  }
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(title: 'Worawit Kimhenglee'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter--;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Worawit Kimhenglee Count Down:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
