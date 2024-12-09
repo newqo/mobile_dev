b8674c2023b09ab6ac0470b8e0f86064409e2417
newqo <worawit.forwork@gmail.com>
2024-12-09 11:07:03 +0700

    commit widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 }
 
 // stl shortcut
+// MyApp is also wiget.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,16 +20,150 @@ class MyApp extends StatelessWidget {
 
       // theme
       theme: ThemeData(
-          useMaterial3: true,
-          appBarTheme: AppBarTheme(color: Colors.lightBlue)),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(color: Colors.lightBlue),
+        // appBarTheme is a color of app bar theme (cover all page)
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        // colorSchema is a color of application that control the body if doesn't have it will use default color(white).
+      ),
 
       home: Scaffold(
         // Scaffold is like form.
+        // property : wiget
         appBar: AppBar(
-          title: Text('Appbar title'),
+          title: const Text('Appbar'),
+          // action is one or more child but leading is have one child only.
+          leading: IconButton(
+            onPressed: () {
+              debugPrint("leading is clicked.");
+            },
+            icon: const Icon(Icons.menu),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                debugPrint("inbox action button is clicked.");
+              },
+              icon: const Icon(Icons.inbox),
+            ),
+            IconButton(
+              onPressed: () {
+                debugPrint("Profile action button is clicked.");
+              },
+              icon: const Icon(Icons.person),
+            ),
+          ],
         ),
-        body: Text(
-            'Hello world'), // if have button or elements it will be paste in body.
+        // body must have 1 wiget(child) only if want to add other wiget must use wrapper ( Column , Row or etc.) to set layout.
+        // if have button or elements it will be paste in body.
+        body: Column(
+          children: [
+            const Text(
+              "My mobile application",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "Hello mobile app.",
+              style: TextStyle(
+                fontSize: 15,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            // Image.network("https://picsum.photos/250?image=9"),
+            // Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM5kt9cWqI1xeKY9K3eRPq7tDbxKPd05cdiQ&s"),
+            Row(
+              children: [
+                Image.asset(
+                  "assets/images/sheep.jpg",
+                  width: 120,
+                  height: 200,
+                ),
+                const Column(
+                  children: [
+                    Text(
+                      "Paragraph Title",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Text abc 12345 aaaaaaaaaa")
+                  ],
+                )
+              ],
+            ),
+
+            // Image.asset(
+            //   "assets/images/sheep.jpg",
+            //   width: 100,
+            //   height: 120,
+            // ),
+            // Image.asset(
+            //   "assets/images/forest.jpg",
+            //   width: 100,
+            //   height: 120,
+            // ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    debugPrint("clicked button");
+                    // print("clicked button 2");
+                  }, // onPreesed : function
+                  child: const Text(
+                    "Submit",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ), // child is text in button
+                ),
+                IconButton(
+                  onPressed: () {
+                    debugPrint("Icon button is clicked.");
+                  },
+                  icon: const Icon(Icons.rocket_launch_rounded),
+                  iconSize: 40,
+                ),
+              ],
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB is clicked.");
+          },
+          child: const Icon(Icons.shopping_bag_rounded),
+          // const Text(
+          //   "FAB",
+          //   style: TextStyle(
+          //     fontSize: 18,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+        ),
+
+        // const Column(
+        //   children: [
+        //     Text(
+        //       'Hello world',
+        //       style: TextStyle(
+        //         fontSize: 25,
+        //         fontWeight: FontWeight.bold,
+        //       ),
+        //     ),
+        //     Text(
+        //       "my mobile app",
+        //       style: TextStyle(
+        //         fontSize: 20,
+        //         fontWeight: FontWeight.normal,
+        //       ),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
